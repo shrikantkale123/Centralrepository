735503263d418bcd213dcab60d9989e9363afdca
Shrikant-PC <Shrikant-PC@192.168.1.39>
2022-11-28 00:24:43 +0530

    Added Test2class

---
+++ Test_Git/src/gitdemopackage/Test2class.java
@@ -0,0 +1,11 @@
+package gitdemopackage;
+
+public class Test2class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test 2 push succesfully");
+
+	}
+
+}
