aabe44b055ea96a370164831d39c9a51ac63dcfb
Shrikant-PC <Shrikant-PC@192.168.1.39>
2022-11-28 00:07:54 +0530

    1st commit

---
+++ Test_Git/src/gitdemopackage/Gittestclass.java
@@ -0,0 +1,11 @@
+package gitdemopackage;
+
+public class Gittestclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Demo of Git");
+
+	}
+
+}
